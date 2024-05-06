855698912a83aee72042b1a6f882d40de80df1d6
Quentin <quentin.rivollat@protonmail.com>
2024-05-06 18:54:10 +0200

    start login

--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,7 @@
+import os 
+
+print("*****")
+print(os.path.join(os.path.dirname(os.path.abspath(__file__))))
+
 SECRET_KEY = 'production_secret'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///prod.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../data', 'app.db'))
