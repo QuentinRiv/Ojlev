1fc4b1384dc48da3b50f5f3447597840ecc3abc7
Quentin <quentin.rivollat@protonmail.com>
2024-08-17 08:40:45 +0200

    debug mode

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ load_dotenv()  # Cela charge les variables d'environnement depuis le fichier .en
 from ojlevapp import create_app
 
 app = create_app()
-
+app.debug = True
 # Plus besoin de app.run ici, Gunicorn s'en occupera
