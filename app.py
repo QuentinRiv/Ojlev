bacb01a852a642b577741fe6e8a85dc42e559d3a
root <quentin.rivollat@protonmail.com>
2024-08-17 06:12:44 +0000

    Initial commit with eisting project

---
+++ app.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+from dotenv import load_dotenv
+load_dotenv()  # Cela charge les variables d'environnement depuis le fichier .env
+
+from ojlevapp import create_app
+
+app = create_app()
+
+# Plus besoin de app.run ici, Gunicorn s'en occupera
