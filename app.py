57b8207a5a38bfa0ad5f211c46779dfa913bc641
Quentin <Quentin.Rivollat@etu.unige.ch>
2024-09-06 00:11:57 +0200

    df

--- app.py
+++ app.py
@@ -2,11 +2,17 @@
 import logging
 from dotenv import load_dotenv
 load_dotenv()  # Cela charge les variables d'environnement depuis le fichier .env
+from flask_compress import Compress
 
 from ojlevapp import create_app
 
 app = create_app()
-app.debug = True
+# app.debug = True
+
+compress = Compress()
+compress.init_app(app)
+app.config['COMPRESS_LEVEL'] = 6  # Niveau de compression de 1 (faible) à 9 (élevé)
+app.config['COMPRESS_MIN_SIZE'] = 500  # Ne pas compresser les fichiers de moins de 500 octets
 
 
 logging.basicConfig(filename='app.log', level=logging.INFO,
@@ -15,5 +21,5 @@ logging.basicConfig(filename='app.log', level=logging.INFO,
 logging.info("Application has been restarted")
 # Plus besoin de app.run ici, Gunicorn s'en occupera
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
